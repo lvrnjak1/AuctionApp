dd558461a01f72716f5f419f29fa52df952ba3f3
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-15 21:39:09 +0100

    Set up MVC Configurer to enable cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGavel } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/health`,).then((res) => {
+      setMessage(res.data);
+    })
+  }, []);
+
   return (
-    <div>Auction App</div>
+    <div>{message}</div>
   );
 }
 
