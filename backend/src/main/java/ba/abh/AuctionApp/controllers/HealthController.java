b7df83251002f4b35ec3ec7fd338a7e2182a978e
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-15 20:34:59 +0100

    Set up swagger and /health endpoint

---
+++ backend/src/main/java/ba/abh/AuctionApp/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package ba.abh.AuctionApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping
+    public ResponseEntity<String> checkApi(){
+        return ResponseEntity.ok().body("Server says HI");
+    }
+}
