2cb511b560d07f7a35f9c2a346050cb09d0ba09d
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-16 19:30:39 +0100

    Create privacy policy page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { ThemeProvider } from '@material-ui/styles';
 import theme from "./util/theme";
 import Layout from './components/main_layout';
+// import Terms from './components/static_pages/terms';
+import Privacy from './components/static_pages/privacy';
 // import NotFound from './components/static_pages/not_found';
 import About from './components/static_pages/about';
 
@@ -9,7 +11,6 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        {/* <NotFound/> */}
         <About />
       </Layout>
     </ThemeProvider>
---
+++ frontend/src/components/static_pages/terms/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./index.css";
+
+function Terms() {
+    return (
+        <div id="content">
+            <p id="content-title">Terms and Conditions</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu. Cras semper auctor neque vitae tempus. Gravida cum sociis natoque penatibus et magnis dis. Enim facilisis gravida neque convallis a. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Aliquet enim tortor at auctor urna nunc id cursus metus. Urna molestie at elementum eu facilisis. Fringilla ut morbi tincidunt augue interdum velit euismod in. Morbi tincidunt ornare massa eget. Amet venenatis urna cursus eget nunc scelerisque. Vel risus commodo viverra maecenas accumsan lacus vel facilisis. Potenti nullam ac tortor vitae purus faucibus ornare. Sed nisi lacus sed viverra tellus. Amet venenatis urna cursus eget nunc scelerisque. Arcu vitae elementum curabitur vitae. In hendrerit gravida rutrum quisque non tellus orci ac.</p>
+            <p id="content-subtitle">Terms</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu. Cras semper auctor neque vitae tempus. Gravida cum sociis natoque penatibus et magnis dis. Enim facilisis gravida neque convallis a. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Aliquet enim tortor at auctor urna nunc id cursus metus. Urna molestie at elementum eu facilisis. Fringilla ut morbi tincidunt augue interdum velit euismod in. Morbi tincidunt ornare massa eget. Amet venenatis urna cursus eget nunc scelerisque. Vel risus commodo viverra maecenas accumsan lacus vel facilisis. Potenti nullam ac tortor vitae purus faucibus ornare. Sed nisi lacus sed viverra tellus. Amet venenatis urna cursus eget nunc scelerisque. Arcu vitae elementum curabitur vitae. In hendrerit gravida rutrum quisque non tellus orci ac.</p>
+            <p id="content-subtitle">Conditions</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam nulla facilisi cras fermentum odio eu. Cras semper auctor neque vitae tempus. Gravida cum sociis natoque penatibus et magnis dis. Enim facilisis gravida neque convallis a. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Aliquet enim tortor at auctor urna nunc id cursus metus. Urna molestie at elementum eu facilisis. Fringilla ut morbi tincidunt augue interdum velit euismod in. Morbi tincidunt ornare massa eget. Amet venenatis urna cursus eget nunc scelerisque. Vel risus commodo viverra maecenas accumsan lacus vel facilisis. Potenti nullam ac tortor vitae purus faucibus ornare. Sed nisi lacus sed viverra tellus. Amet venenatis urna cursus eget nunc scelerisque. Arcu vitae elementum curabitur vitae. In hendrerit gravida rutrum quisque non tellus orci ac.</p>
+        </div>
+    );
+}
+
+export default Terms;
