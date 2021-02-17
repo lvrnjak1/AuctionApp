985cbe35b09bf61ec3a42f2126cb362badcdd6ea
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-17 22:51:19 +0100

    Fix heights for responsive layout

--- frontend/src/components/layout/layout.js
+++ frontend/src/components/layout/layout.js
@@ -5,17 +5,17 @@ import { Grid, makeStyles } from '@material-ui/core';
 import SearchBar from 'components/header/search_bar/searchBar';
 import BreadcrumbBar from 'components/header/breadcrumb_bar/breadcrumbBar';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     container: {
         width: "100%"
     },
-    navbar: {
-        // height: 40
-    },
     searchbar: {
-        // height: 60
+        height: 100,
+        [theme.breakpoints.up('md')]: {
+            height: 60,
+        }
     }
-})
+}))
 
 function Layout(props) {
     const classes = useStyles();
@@ -25,7 +25,7 @@ function Layout(props) {
             direction="column" className={classes.container}>
             {!props.removeHeader ?
                 <>
-                    <Grid item className={classes.navbar}><Navbar /></Grid>
+                    <Grid item><Navbar /></Grid>
                     <Grid item className={classes.searchbar}><SearchBar /></Grid>
                     <Grid item><BreadcrumbBar content={props.breadcrumbs} /></Grid>
                 </> : ""}
