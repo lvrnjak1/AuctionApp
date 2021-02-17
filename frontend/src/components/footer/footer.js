e57ca79fbedd5c6deed74485a04c3b0bed78495e
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-17 16:24:10 +0100

    Create footer with scss

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,61 +1,56 @@
 import React from 'react'
-import { Grid, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faTwitter, faGooglePlus } from "@fortawesome/free-brands-svg-icons";
-import style from './footerStyle';
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import "./footer.scss";
 
-const useStyles = makeStyles(style)
+const useStyles = makeStyles({
+    link: {
+        textDecoration: "none",
+        color: "#FFFFFF"
+    },
+    icon: {
+        color: "#9B9B9B",
+        marginInline: 3,
+        fontSize: 20
+    }
+})
 
 function Footer() {
     const classes = useStyles();
     return (
-        <Container className={classes.root} maxWidth="xl">
-            <Grid container justify="center" className={classes.footer}>
-                <Grid item xs={3} />
-                <Grid item className={classes.content} xs={3}>
-                    <div>
-                        <div className={classes.contentDiv}>AUCTION</div>
-                        <ul className={classes.contentList}>
-                            <li className={classes.contentListItem}>
-                                <Link to="/shop/about" className={classes.headerItemLink}>About Us</Link>
-                            </li>
-                            <li className={classes.contentListItem}>
-                                <Link to="/shop/terms" className={classes.headerItemLink}>Terms and Conditions</Link>
-                            </li>
-                            <li className={classes.contentListItem}>
-                                <Link to="/shop/privacy" className={classes.headerItemLink}>Privacy and Policy</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </Grid>
-                <Grid item className={classes.content} xs={3} >
-                    <div>
-                        <div className={classes.contentDiv}>GET IN TOUCH</div>
-                        <ul className={classes.contentList}>
-                            <li className={classes.contentListItem}>Call Us at +123 797-567-2535</li>
-                            <li className={classes.contentListItem}>support@auction.com</li>
-                        </ul>
-                        <div className={classes.contentDiv}>
-                            <Link to="/home" className={classes.headerItemLink}>
-                                <FontAwesomeIcon icon={faFacebook} className={classes.headerIcon} />
-                            </Link>
-                            <Link to="/home" className={classes.headerItemLink}>
-                                <FontAwesomeIcon icon={faInstagram} className={classes.headerIcon} />
-                            </Link>
-                            <Link to="/home" className={classes.headerItemLink}>
-                                <FontAwesomeIcon icon={faTwitter} className={classes.headerIcon} />
-                            </Link>
-                            <Link to="/home" className={classes.headerItemLink}>
-                                <FontAwesomeIcon icon={faGooglePlus} className={classes.headerIcon} />
-                            </Link>
-                        </div>
-                    </div>
-                </Grid>
-                <Grid item xs={3} />
-            </Grid>
-        </Container>
+        <div id="footer">
+            <div id="content-left">
+                <p>AUCTION</p>
+                <ul>
+                    <li><NavLink to="/shop/about" className={classes.link}>About Us</NavLink></li>
+                    <li><NavLink to="/shop/terms" className={classes.link}>Terms and Conditions</NavLink></li>
+                    <li><NavLink to="/shop/privacy" className={classes.link}>Privacy and Policy</NavLink></li>
+                </ul>
+            </div>
+            <div id="content-right">
+                <p>GET IN TOUCH</p>
+                <ul>
+                    <li >Call Us at +123 797-567-2535</li>
+                    <li>support@auction.com</li>
+                </ul>
+                <div >
+                    <NavLink to="/home">
+                        <FontAwesomeIcon icon={faFacebook} className={classes.icon} />
+                    </NavLink>
+                    <NavLink to="/home">
+                        <FontAwesomeIcon icon={faInstagram} className={classes.icon} />
+                    </NavLink>
+                    <NavLink to="/home">
+                        <FontAwesomeIcon icon={faTwitter} className={classes.icon} />
+                    </NavLink>
+                    <NavLink to="/home">
+                        <FontAwesomeIcon icon={faGooglePlus} className={classes.icon} />
+                    </NavLink>
+                </div>
+            </div>
+        </div>
     )
 }
 
