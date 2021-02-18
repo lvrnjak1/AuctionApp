dfed7af331185310d3a1bd7bb8d8121e34dac387
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-18 16:54:31 +0100

    Implement a secured health enpoint and minor fixes

--- backend/src/main/java/ba/abh/AuctionApp/controllers/HealthController.java
+++ backend/src/main/java/ba/abh/AuctionApp/controllers/HealthController.java
@@ -1,6 +1,7 @@
 package ba.abh.AuctionApp.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 public class HealthController {
     @GetMapping
+    @Secured("ROLE_BUYER")
     public ResponseEntity<String> checkApi(){
         return ResponseEntity.ok().body("Server says HI");
     }
