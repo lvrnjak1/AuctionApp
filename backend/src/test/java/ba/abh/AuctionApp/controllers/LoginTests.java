31aee149e4c9ce7f315e0ee7066b6ec34f391e3b
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-18 23:06:12 +0100

    Set profile to prod and add Login tests

--- backend/src/test/java/ba/abh/AuctionApp/controllers/LoginTests.java
+++ backend/src/test/java/ba/abh/AuctionApp/controllers/LoginTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -26,4 +27,51 @@ public class LoginTests {
                         "}"))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void testLoginNoUserWithMail() throws Exception {
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"email\": \"lami.vrnjak@gmail.com\",\n" +
+                        "    \"password\": \"password\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message",
+                        is("User with email lami.vrnjak@gmail.com doesn't exist")));
+    }
+
+    @Test
+    public void testLoginWrongPassword() throws Exception {
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"email\": \"lamija.vrnjak@gmail.com\",\n" +
+                        "    \"password\": \"password123\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message", is("Invalid credentials")));
+    }
+
+    @Test
+    public void testLoginNoEmail() throws Exception {
+        mockMvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"password\": \"password123\"\n" +
+                        "}"))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("message", is("email must not be blank")));
+    }
+
+//    @Test
+//    public void testLoginNoPassword() throws Exception {
+//        mockMvc.perform(post("/auth/login")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content("{\n" +
+//                        "    \"email\": \"lamija.vrnjak@gmail.com\",\n" +
+//                        "}"))
+//                .andExpect(status().isUnprocessableEntity())
+//                .andExpect(jsonPath("message", is("password should not be blank")));
+//    }
 }
