505e4ce8a966a8ab68c47372a7daf93e91e615a4
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-19 19:53:59 +0100

    Implement register on frontend

--- frontend/src/components/forms/register/register.js
+++ frontend/src/components/forms/register/register.js
@@ -1,22 +1,80 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import "components/forms/forms.scss";
+import axios from 'axios';
+import { REGISTER_ENDPOINT } from 'util/endpoints';
 
-function Register() {
+function Register(props) {
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [messageStyleClass, setMessageStyleClass] = useState("");
+
+    const history = useHistory();
+
+    const setInfoMessage = (message, className = "") => {
+        setMessage(message);
+        setMessageStyleClass(className);
+    }
+
+    const handleInputChange = (e, setter) => {
+        setter(e.target.value);
+        setInfoMessage("");
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const registerBody = {
+            name, surname, email, password
+        }
+
+        try {
+            await axios.post(REGISTER_ENDPOINT, registerBody);
+            history.push("/login");
+        } catch (error) {
+            setInfoMessage(error.response.data.message, "error");
+        }
+    }
+
     return (
-        <form className="register-form">
+        <form className="register-form" onSubmit={handleSubmit}>
             <p className="title">REGISTER</p>
             <div className="form-content">
-                <label for="name">First name</label>
-                <input type="text" id="name" />
+                <label>First name</label>
+                <input
+                    required
+                    type="text"
+                    value={name}
+                    onChange={e => handleInputChange(e, setName)} />
+
                 <label>Last name</label>
-                <input type="text" />
+                <input
+                    required
+                    type="text"
+                    value={surname}
+                    onChange={e => handleInputChange(e, setSurname)} />
+
                 <label>Email</label>
-                <input type="email" />
+                <input
+                    required
+                    type="email"
+                    value={email}
+                    onChange={e => handleInputChange(e, setEmail)} />
+
                 <label>Password</label>
-                <input type="password" />
+                <input
+                    required
+                    minLength={5}
+                    type="password"
+                    value={password}
+                    onChange={e => handleInputChange(e, setPassword)} />
+
                 <button type="submit">REGISTER</button>
                 <p>Already have an account? <NavLink to="/login" className="link">Login</NavLink></p>
+                <p className={`${messageStyleClass}`}>{message}</p>
             </div>
         </form>
     );
---
+++ frontend/src/util/endpoints.js
@@ -0,0 +1,1 @@
+export const REGISTER_ENDPOINT = `${process.env.REACT_APP_API_URL}/auth/register`;
