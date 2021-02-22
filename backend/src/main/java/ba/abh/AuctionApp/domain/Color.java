3f9ae5a393514f26ed0c7db9ea50b4b4355989de
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-02-22 20:27:23 +0100

    Create script for all tables and insert colors, represent colors as enums

--- backend/src/main/java/ba/abh/AuctionApp/domain/Color.java
+++ backend/src/main/java/ba/abh/AuctionApp/domain/Color.java
@@ -1,43 +1,32 @@
 package ba.abh.AuctionApp.domain;
 
-import javax.persistence.Column;
+import ba.abh.AuctionApp.domain.enums.ColorName;
+import org.hibernate.annotations.NaturalId;
+
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
-import java.util.Objects;
 
 @Entity
 @Table(name = "colors")
 public class Color extends BaseEntity{
-    @Column(unique = true)
-    private String color;
+    @Enumerated(value = EnumType.STRING)
+    @NaturalId
+    private ColorName color;
 
     public Color() {
     }
 
-    public Color(final Long id, final String color) {
-        super(id);
+    public Color(final ColorName color) {
         this.color = color;
     }
 
-    public String getColor() {
+    public ColorName getColor() {
         return color;
     }
 
-    public void setColor(final String color) {
+    public void setColor(final ColorName color) {
         this.color = color;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Color color1 = (Color) o;
-        return Objects.equals(color, color1.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), color);
     }
 }
--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,12 @@
-drop table if exists roles cascade;
-drop table if exists user_roles cascade;
-drop table if exists users cascade;
+drop table if exists product_colors;
+drop table if exists images;
+drop table if exists auctions;
+drop table if exists products;
+drop table if exists colors;
+drop table if exists categories;
+drop table if exists user_roles;
+drop table if exists roles;
+drop table if exists users;
 
 create table roles
 (
@@ -24,6 +30,53 @@ create table user_roles
     constraint user_roles_pkey primary key (user_id, role_id)
 );
 
+create table categories
+(
+    id bigint not null constraint categories_pkey primary key,
+    c_left integer not null,
+    c_right integer not null,
+    name varchar(255) not null
+);
+
+create table colors
+(
+    id bigint not null constraint colors_pkey primary key,
+    color varchar(255) constraint uk_colors_color unique
+);
+
+create table products
+(
+    id bigint not null constraint products_pkey primary key,
+    description varchar(255),
+    name varchar(255) not null,
+    size varchar(255),
+    category_id bigint constraint fk_category references categories
+);
+
+create table auctions
+(
+    id bigint not null constraint auctions_pkey primary key,
+    end_date_time timestamp,
+    start_date_time timestamp,
+    start_price numeric(19, 2),
+    product_id bigint constraint fk_product references products,
+    seller_id bigint constraint fk_user references users
+);
+
+create table images
+(
+    id bigint not null constraint images_pkey primary key,
+    image oid not null,
+    product_id bigint constraint fk_product references products
+);
+
+create table product_colors
+(
+    product_id bigint not null constraint fk_product references products,
+    color_id bigint not null constraint fk_color references colors,
+    constraint product_colors_pkey primary key (product_id, color_id)
+);
+
 insert into roles(id, role) values (1, 'ROLE_BUYER');
 insert into roles(id, role) values (2, 'ROLE_SELLER');
 
@@ -37,3 +90,16 @@ values (3, 'paja.patak@gmail.com', 'Paja', 'Patak', '$2a$10$DXLs8e3zTsfPcYWdYUrh
 insert into user_roles (user_id, role_id) values (1,1);
 insert into user_roles (user_id, role_id) values (2,1);
 insert into user_roles (user_id, role_id) values (3,1);
+insert into user_roles (user_id, role_id) values (1,2);
+
+insert into colors (id, color) values (1, 'BLACK');
+insert into colors (id, color) values (2, 'WHITE');
+insert into colors (id, color) values (3, 'RED');
+insert into colors (id, color) values (4, 'BLUE');
+insert into colors (id, color) values (5, 'GREEN');
+insert into colors (id, color) values (6, 'ORANGE');
+insert into colors (id, color) values (7, 'PINK');
+insert into colors (id, color) values (8, 'PURPLE');
+insert into colors (id, color) values (9, 'YELLOW');
+insert into colors (id, color) values (10, 'GRAY');
+
