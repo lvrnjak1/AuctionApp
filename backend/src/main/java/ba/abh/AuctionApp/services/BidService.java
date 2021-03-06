0e76642d41374110c434e0f0e15bd0fee9cb6136
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-06 10:41:22 +0100

    Implement GET bids for auction

--- backend/src/main/java/ba/abh/AuctionApp/services/BidService.java
+++ backend/src/main/java/ba/abh/AuctionApp/services/BidService.java
@@ -17,9 +17,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Optional;
 
-import ba.abh.AuctionApp.repositories.BidRepository;
-import ba.abh.AuctionApp.requests.BidRequest;
-import org.springframework.stereotype.Service;
 
 @Service
 public class BidService {
