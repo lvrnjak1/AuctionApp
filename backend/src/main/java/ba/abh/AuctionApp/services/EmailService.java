eeb2d2be6c17c084d743e1fb3040e1ff80ef2810
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-16 15:50:29 +0100

    Send template mail

--- backend/src/main/java/ba/abh/AuctionApp/services/EmailService.java
+++ backend/src/main/java/ba/abh/AuctionApp/services/EmailService.java
@@ -15,22 +15,24 @@ import java.io.IOException;
 
 @Service
 public class EmailService {
-    @Value("${app.mail.fromMail}")
-    private String fromMail;
+    @Value("${app.mail.sender}")
+    private String sender;
     @Value("${app.mail.apiKey}")
     private String apiKey;
-    private final String RESET_URL = "https://lvrnjak-auction-app.herokuapp.com/password/reset?token=";
-    private final String TEMPLATE_ID = "d-4690af3c953b43629942fe8c89974abc";
+    @Value("${app.mail.resetUrl}")
+    private String resetUrl;
+    @Value("${app.mail.templateId}")
+    private String templateId;
 
     @Async
     public void sendMail(final Token token) throws IOException {
-        Email fromMail = new Email("lamija.vrnjak@gmail.com");
+        Email fromMail = new Email(sender);
         Email toMail = new Email(token.getUser().getEmail());
         Mail mail = new Mail();
         mail.setFrom(fromMail);
-        mail.setTemplateId(TEMPLATE_ID);
+        mail.setTemplateId(templateId);
         Personalization personalization = new Personalization();
-        personalization.addDynamicTemplateData("reset_url", String.format("%s%s", RESET_URL, token.getToken()));
+        personalization.addDynamicTemplateData("reset_url", String.format("%s%s", resetUrl, token.getToken()));
         personalization.addTo(toMail);
         mail.addPersonalization(personalization);
 
