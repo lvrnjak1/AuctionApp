fb759411f2260eb92fbfa347083420da108f7166
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-23 11:08:05 +0100

    Slider with on commit event

--- frontend/src/components/price_filter/priceFilter.js
+++ frontend/src/components/price_filter/priceFilter.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import "components/price_filter/priceFilter.scss";
-import { Slider } from '@material-ui/core';
+import { debounce, Slider } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { setMaxPrice, setMinPrice } from 'state/actions/filterParamsActions';
 
 function PriceFilter() {
-    const filterParams = useSelector(state => state.filterParams)
     const [value, setValue] = useState([20, 100]);
     const dispatch = useDispatch();
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    const handleChangeCommited = (event, commited) => {
+        dispatch(setMinPrice(commited[0]));
+        dispatch(setMaxPrice(commited[1]));
+    }
 
     function valuetext(value) {
         return `$${value.toFixed(2)}`;
@@ -21,11 +25,6 @@ function PriceFilter() {
         return (value[0] + value[1]) / 2;
     }
 
-    const handleApply = (e) => {
-        dispatch(setMinPrice(value[0]));
-        dispatch(setMaxPrice(value[1]));
-    }
-
     return (
         <div className="price-filter">
             <p className="title">Filter by price</p>
@@ -33,6 +32,7 @@ function PriceFilter() {
                 className="slider"
                 value={value}
                 onChange={handleChange}
+                onChangeCommitted={handleChangeCommited}
                 valueLabelDisplay="auto"
                 aria-labelledby="range-slider"
                 getAriaValueText={valuetext}
@@ -44,7 +44,6 @@ function PriceFilter() {
             <p className="filter-text">
                 {`Average price ${valuetext(average())}`}
             </p>
-            <button className="apply-button" onClick={(e) => handleApply(e)}>Apply</button>
         </div>
     );
 }
