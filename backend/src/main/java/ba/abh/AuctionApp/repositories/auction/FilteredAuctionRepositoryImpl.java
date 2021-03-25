9ebf48ae8d570c731d31ffa7ae5b712f15021623
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-03-25 12:10:49 +0100

    fix yml

--- backend/src/main/java/ba/abh/AuctionApp/repositories/auction/FilteredAuctionRepositoryImpl.java
+++ backend/src/main/java/ba/abh/AuctionApp/repositories/auction/FilteredAuctionRepositoryImpl.java
@@ -239,7 +239,7 @@ public class FilteredAuctionRepositoryImpl implements FilteredAuctionRepository 
         Object[] count = typedCountQuery.getSingleResult();
 
         List<Long> values = new ArrayList<>();
-        if(count != null && count[0] != null && count[1] != null) {
+        if (count != null && count[0] != null && count[1] != null) {
             values = Arrays.stream(count).mapToLong(el -> (long) el).boxed().collect(Collectors.toList());
         }
         return new PriceChartResponse(labels, values, min, max + step, step);
