aa2604876484b03cadc95078c205b91db08c1635
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-04-10 17:21:39 +0200

    Email and phone on settings

--- frontend/src/components/settings/settings.js
+++ frontend/src/components/settings/settings.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import "components/profile/profile.scss";
 import "components/settings/settings.scss";
+import { getRequest } from 'http/requests';
+import { updateMessage } from 'util/info_div_util';
+import { getAuthorizationConfig } from 'util/auth/auth';
+import { USER_PROFILE_ENDPOINT } from 'http/endpoints';
 
 function Settings() {
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    const setUserData = useCallback((userData) => {
+        setEmail(userData.email);
+        setPhoneNumber(userData.phoneNumber);
+    }, [])
+
+    useEffect(() => {
+        let isSubscribed = true;
+        async function getUserProfile() {
+            await getRequest(USER_PROFILE_ENDPOINT,
+                {},
+                (response) => { if (isSubscribed) setUserData(response.data) },
+                () => { if (isSubscribed) updateMessage("Something went wrong, try reloading the page", "error", 3000) },
+                getAuthorizationConfig()
+            );
+        }
+
+        getUserProfile();
+
+        return () => (isSubscribed = false)
+    }, [setUserData])
 
     return (
         <form className="settings">
@@ -10,8 +37,8 @@ function Settings() {
                 <p className="form-title">Contact information</p>
                 <div className="form-content">
                     <p>This information can be edited on your profile.</p>
-                    <p>Email<span className="purple-text">lamija.vrnjak@gmail.com</span></p>
-                    <p>Phone<span className="purple-text">062 348 811</span></p>
+                    <p>Email<span className="purple-text">{email}</span></p>
+                    <p>Phone<span className="purple-text">{phoneNumber}</span></p>
                 </div>
             </div>
 
