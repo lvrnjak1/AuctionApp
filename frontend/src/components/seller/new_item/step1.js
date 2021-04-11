77dda4c43650873babab098668f9244f5e08849f
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-04-12 00:26:46 +0200

    Fix category select

--- frontend/src/components/seller/new_item/step1.js
+++ frontend/src/components/seller/new_item/step1.js
@@ -1,19 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import ImageUploader from "components/seller/image_uploader/imageUploader";
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCategories } from 'state/actions/categoriesActions';
+import { getRequest } from 'http/requests';
+import { CATEGORIES_ENDPOINT } from 'http/endpoints';
+import { updateMessage } from 'util/info_div_util';
 
 function StepOne(props) {
     const categories = useSelector(state => state.categories);
     const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(0);
     const [selectedSubcategoryIndex, setSelectedSubcategoryIndex] = useState(0);
+    const dispatch = useDispatch();
 
     const findIndex = (collection, id) => {
         return collection.findIndex(cat => parseInt(cat.id) === parseInt(id));
     }
 
+    const setCategoriesCallback = useCallback((data) => {
+        dispatch(setCategories(data))
+    }, [dispatch]);
+
     useEffect(() => {
-        props.setSubcategory(categories[selectedCategoryIndex].subcategories[selectedSubcategoryIndex].id);
-    }, [props, props.setSubcategory, selectedCategoryIndex, selectedSubcategoryIndex, categories])
+        async function fetchCategories() {
+            await getRequest(CATEGORIES_ENDPOINT,
+                {},
+                (response) => setCategoriesCallback(response.data),
+                () => updateMessage("Something went wrong. Try reloading the page.", "error"));
+        }
+
+        if (!categories.length) {
+            fetchCategories();
+        }
+    }, [categories.length, setCategoriesCallback]);
 
     return (
         <form className="form" onSubmit={props.handleNext}>
@@ -34,6 +52,7 @@ function StepOne(props) {
                             required
                             className="input"
                             value={categories[selectedCategoryIndex].id}
+                            defaultValue={categories[0].id}
                             onChange={e => {
                                 props.setCategory(e.target.value);
                                 const index = findIndex(categories, e.target.value);
@@ -50,13 +69,12 @@ function StepOne(props) {
                             required
                             className="input"
                             value={categories[selectedCategoryIndex].subcategories[selectedSubcategoryIndex].id}
+                            defaultValue={categories[0].subcategories[0].id}
                             onChange={e => {
                                 props.setSubcategory(e.target.value);
                                 const index = findIndex(categories[selectedCategoryIndex].subcategories, e.target.value);
                                 setSelectedSubcategoryIndex(index);
                             }}>
-                            {/* {categories[selectedCategoryIndex].subcategories.length == 0 ?
-                                <option value="" disabled>Subcategory</option> : ""} */}
                             {categories[selectedCategoryIndex].subcategories.map(sc => {
                                 return <option key={sc.id} value={sc.id}>{sc.name}</option>
                             })}
