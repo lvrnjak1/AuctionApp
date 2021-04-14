c7f6aef162d22c5674a2948fffa57f49f219d670
lvrnjak1 <44277608+lvrnjak1@users.noreply.github.com>
2021-04-15 01:54:46 +0200

    Seed products (#30)
    * Load 8 products at once
    
    * Insert more products to database
    
    * Fix tests

---
+++ backend/src/main/resources/db/migration/V3__new_products.sql
@@ -0,0 +1,148 @@
+insert into products (id, description, name, size, category_id)
+values (9, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ' ||
+           'et dolore magna aliqua. Varius quam quisque id diam vel.', 'Minimal UNO Set', null, 23);
+insert into images (id, image_url, product_id)
+values (39, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441249/fadel-atthoriq-kzGwyFEtdFM-unsplash_ykmeax.jpg', 9);
+insert into images (id, image_url, product_id)
+values (40, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441250/fadel-atthoriq-oy1C8VRGrzA-unsplash_nnjtik.jpg', 9);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (9, '2021-05-20 13:30:00.000000', '2021-03-25 11:00:00.000000', 50, 9, 7);
+
+insert into products (id, description, name, size, category_id)
+values (10, 'Ultrices in iaculis nunc sed. Hendrerit gravida rutrum quisque non tellus. ' ||
+            'Nam aliquam sem et tortor consequat id. Vitae justo eget magna fermentum iaculis eu non.',
+        'Oriental Women Bandana', null, 7);
+insert into images (id, image_url, product_id)
+values (41, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441232/courtney-smith-ZXDFXoq1k0k-unsplash_z2q5jc.jpg', 10);
+insert into images (id, image_url, product_id)
+values (42, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441234/courtney-smith-l2NryXUJFaY-unsplash_sjob7b.jpg', 10);
+insert into images (id, image_url, product_id)
+values (43, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441234/courtney-smith-ZXDFXoq1k0k-unsplash_1_jru9pu.jpg', 10);
+insert into images (id, image_url, product_id)
+values (44, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441234/courtney-smith-rCsjMEnetkU-unsplash_lwzcda.jpg', 10);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (10, '2021-05-17 17:30:00.000000', '2021-04-05 10:30:00.000000', 13.75, 10, 7);
+
+insert into products (id, description, name, size, category_id)
+values (11, 'Placerat orci nulla pellentesque dignissim. ' ||
+            'Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Integer enim neque volutpat ac.',
+        'Minimal Black T-shirt', null, 9);
+insert into images (id, image_url, product_id)
+values (45, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441236/faith-yarn-Wr0TpKqf26s-unsplash_ewfxsc.jpg', 11);
+insert into images (id, image_url, product_id)
+values (46, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441237/faith-yarn-hgtWvsq5e2c-unsplash_vzlciw.jpg', 11);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (11, '2021-05-23 17:30:00.000000', '2021-04-10 10:30:00.000000', 30, 11, 7);
+
+insert into products (id, description, name, size, category_id)
+values (12, 'Aenean euismod elementum nisi quis eleifend quam. Sem nulla pharetra diam sit amet nisl. ' ||
+            'Condimentum mattis pellentesque id nibh tortor', 'Tiny Playing Cars', null, 18);
+insert into images (id, image_url, product_id)
+values (47, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441252/vlad-zinculescu-5-qW6-X8mDs-unsplash_z8jl01.jpg', 12);
+insert into images (id, image_url, product_id)
+values (48, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441251/girl-with-red-hat-Xxg-r574pLE-unsplash_ut3krm.jpg', 12);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (12, '2021-05-20 18:30:00.000000', '2021-04-11 10:30:00.000000', 35, 12, 5);
+
+insert into products (id, description, name, size, category_id)
+values (13, 'Aliquam eleifend mi in nulla posuere sollicitudin. Enim diam vulputate ut pharetra sit amet aliquam. ' ||
+            'Dignissim sodales ut eu sem integer vitae justo eget magna. Sit amet mattis vulputate enim nulla aliquet.',
+        'Thin Feminine Gold Jewelry Set', null, 7);
+insert into images (id, image_url, product_id)
+values (49, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441253/joeyy-lee-hXhMzWysB8s-unsplash_esi8sr.jpg', 13);
+insert into images (id, image_url, product_id)
+values (50, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441253/joeyy-lee-onU-LlrPs8I-unsplash_tmryi5.jpg', 13);
+insert into images (id, image_url, product_id)
+values (51, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441252/joeyy-lee-BVQjyuTP1Ho-unsplash_clje7r.jpg', 13);
+insert into images (id, image_url, product_id)
+values (52, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441252/olive-MDFhTpNCfks-unsplash_kbwoli.jpg', 13);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (13, '2021-05-23 17:30:00.000000', '2021-04-10 10:30:00.000000', 60, 13, 5);
+
+insert into products (id, description, name, size, category_id)
+values (14, 'Metus dictum at tempor commodo. Egestas dui id ornare arcu. Facilisi nullam vehicula ' ||
+            'ipsum a arcu cursus vitae. Consectetur lorem donec massa sapien faucibus et molestie ac. ',
+        'Casual Orange And Beige Sneakers', null, 17);
+insert into images (id, image_url, product_id)
+values (53, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441255/mojtaba-fahiminia-T9IqDhJVhEU-unsplash_d32y2w.jpg', 14);
+insert into images (id, image_url, product_id)
+values (54, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441255/mojtaba-fahiminia-t4g1gctAaKk-unsplash_uhmjac.jpg', 14);
+insert into images (id, image_url, product_id)
+values (55, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441254/mojtaba-fahiminia-q7lul0zB0Lg-unsplash_nt6fdv.jpg', 14);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (14, '2021-05-25 17:30:00.000000', '2021-04-03 10:30:00.000000', 125, 14, 5);
+
+insert into products (id, description, name, size, category_id)
+values (15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et ' ||
+            'dolore magna aliqua. Vulputate eu scelerisque felis imperdiet.', 'Small Designer Bag', null, 8);
+insert into images (id, image_url, product_id)
+values (56, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441263/arno-senoner-HFE2RyC76tw-unsplash_zppeii.jpg', 15);
+insert into images (id, image_url, product_id)
+values (57, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441236/arno-senoner-oCXVxwTFwqE-unsplash_wkonpl.jpg', 15);
+insert into images (id, image_url, product_id)
+values (58, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441236/arno-senoner-ZT16YkAYueo-unsplash_rtcuqq.jpg', 15);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (15, '2021-05-30 17:30:00.000000', '2021-04-03 10:30:00.000000', 355, 15, 5);
+
+insert into products (id, description, name, size, category_id)
+values (16, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et' ||
+            ' dolore magna aliqua. Nisl nunc mi ipsum faucibus. ', 'Floral red belted midi dress', null, 9);
+insert into images (id, image_url, product_id)
+values (59, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441250/ussama-azam-5IcEBmSOQq0-unsplash_rghshc.jpg', 16);
+insert into images (id, image_url, product_id)
+values (60, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441250/ussama-azam-HKchJL3uFrw-unsplash_npew4b.jpg', 16);
+insert into images (id, image_url, product_id)
+values (61, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441250/ussama-azam-xgNSlx7DjYM-unsplash_odohei.jpg', 16);
+insert into images (id, image_url, product_id)
+values (62, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441248/ussama-azam-xgNSlx7DjYM-unsplash_1_lkvpcp.jpg', 16);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (16, '2021-05-24 12:30:00.000000', '2021-04-08 16:30:00.000000', 100, 16, 3);
+
+insert into products (id, description, name, size, category_id)
+values (17, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' ||
+            'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', 'Beige Long Autumn Coat', null, 14);
+insert into images (id, image_url, product_id)
+values (63, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441245/austin-wade-iQn82USu8gs-unsplash_ceqsmt.jpg', 17);
+insert into images (id, image_url, product_id)
+values (64, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441243/austin-wade-d2s8NQ6WD24-unsplash_wogaas.jpg', 17);
+insert into images (id, image_url, product_id)
+values (65, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441243/austin-wade-lNy-Yi0U-3o-unsplash_xhv4wv.jpg', 17);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (17, '2021-05-19 12:30:00.000000', '2021-04-11 16:30:00.000000', 135, 17, 3);
+
+insert into products (id, description, name, size, category_id)
+values (18, 'Arcu vitae elementum curabitur vitae nunc sed. Dictum varius duis at consectetur lorem donec massa. ' ||
+            'Consectetur adipiscing elit ut aliquam purus sit amet. ', 'Long Minimal Glass Vase', null, 23);
+insert into images (id, image_url, product_id)
+values (66, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441240/oriento-6HYqdm0CniQ-unsplash_hga0vm.jpg', 18);
+insert into images (id, image_url, product_id)
+values (67, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441240/oriento-x0XIS-iPM4o-unsplash_yhn4jh.jpg', 18);
+insert into images (id, image_url, product_id)
+values (68, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441239/oriento-QRMMG3p7VCA-unsplash_dxqhry.jpg', 18);
+insert into images (id, image_url, product_id)
+values (69, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441239/oriento-S_ncR5Q41PI-unsplash_l7zdkj.jpg', 18);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (18, '2021-05-19 12:30:00.000000', '2021-04-11 16:30:00.000000', 42.50, 18, 3);
+
+insert into products (id, description, name, size, category_id)
+values (19, ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ' ||
+            'Excepteur sint occaecat cupidatat non proident.',
+        'Formal Classic Black High Heel', null, 12);
+insert into images (id, image_url, product_id)
+values (70, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441230/laura-chouette-AlX5260JnmI-unsplash_hxiuyx.jpg', 19);
+insert into images (id, image_url, product_id)
+values (71, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441231/laura-chouette-C83wTawvoio-unsplash_mcjfgz.jpg', 19);
+insert into images (id, image_url, product_id)
+values (72, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441231/laura-chouette-EZXKn9P2scc-unsplash_gqtfqb.jpg', 19);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (19, '2021-05-19 12:30:00.000000', '2021-04-11 16:30:00.000000', 93.99, 19, 1);
+
+insert into products (id, description, name, size, category_id)
+values (20, 'Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Elit pellentesque habitant morbi tristique senectus et.',
+        'Modern Thin Frame Reading Glasses', null, 7);
+insert into images (id, image_url, product_id)
+values (73, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441251/libby-penner-rTjFwFmha9E-unsplash_ckwamt.jpg', 20);
+insert into images (id, image_url, product_id)
+values (74, 'https://res.cloudinary.com/lvrnjak/image/upload/v1618441251/sincerely-media-BfriYg0iOCs-unsplash_y0kdhc.jpg', 20);
+insert into auctions (id, end_date_time, start_date_time, start_price, product_id, seller_id)
+values (20, '2021-05-19 12:30:00.000000', '2021-03-11 16:30:00.000000', 54.50, 20, 1);
