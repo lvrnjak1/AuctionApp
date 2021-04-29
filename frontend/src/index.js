32a4146fe5fe3f87f94b639e1e8028f65300faf2
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-04-29 12:29:48 +0200

    Include stripe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,16 @@ import 'index.css';
 import App from 'App';
 import { Provider } from 'react-redux';
 import store from "state/store";
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Elements stripe={stripePromise}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Elements>,
   document.getElementById('root')
 );
