dfdd7ba9facaa8d7942a43c3bbac0fa3e2a0defc
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2021-05-04 11:35:38 +0200

    Refetch after item is placed in wishlist

--- frontend/src/components/wishlist/wishlistButton.js
+++ frontend/src/components/wishlist/wishlistButton.js
@@ -6,15 +6,20 @@ import { putRequest } from "http/requests";
 import { AUCTIONS_ENDPOINT } from 'http/endpoints';
 import { updateMessage } from 'util/info_div_util';
 import { getAuthorizationConfig, getToken } from 'util/auth/auth';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPage } from 'state/actions/filterParamsActions';
 
 function WishlistButton(props) {
     const [isInWishlist, setIsInWishlist] = useState(false);
     const [message, setMessage] = useState("");
     const [messageVisible, setMessageVisible] = useState(false);
+    const dispatch = useDispatch();
+    const filterParams = useSelector(state => state.filterParams)
 
     useEffect(() => {
         setIsInWishlist(props.inWishlist);
-    }, [props.inWishlist]);
+        dispatch(setPage(filterParams.page));
+    }, [props.inWishlist, dispatch, filterParams.page]);
 
     useEffect(() => {
         setMessageVisible(props.message);
